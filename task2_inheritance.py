063bed027ca39d6b2979a5cae95ba37a0e1420e3
jonibek99 <jonibekannaev5@gmail.com>
2025-05-09 18:37:55 +0500

    oop2

--- task2_inheritance.py
+++ task2_inheritance.py
@@ -14,3 +14,22 @@ Create a subclass called Student that inherits from Person:
 - Override the __str__ method to include the grade
   (e.g., "Student(name=name, age=age, grade=grade)")
 """
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def greet(self):
+        print(f" Hello my name is {self.name}")
+    def __str__(self):
+        return f"Person (name={self.name}, age{self.age})"
+class Student(Person):
+    def __init__(self, name, age,grade):
+        self.grade=grade
+        super().__init__(name, age)
+    def study(self):
+        return f"Student {self.name} is studying."
+    def __str__(self):
+        return f"Student(name={self.name}, age={self.age}, grade={self.grade})"
+b=Student('malika',13,50)
+b.greet()
+print(b)
